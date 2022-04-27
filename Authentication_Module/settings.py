b7656e49f163d76c7edef12615c7af2aef2a062c
Rahmat <srshakir98@gmail.com>
2022-04-27 20:06:52 +0530

    changes in settings.py

--- Authentication_Module/settings.py
+++ Authentication_Module/settings.py
@@ -59,6 +59,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+MIDDLEWARE_CLASSES = (
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 ROOT_URLCONF = 'Authentication_Module.urls'
 
 TEMPLATES = [
