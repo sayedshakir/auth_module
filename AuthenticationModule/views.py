498d3a2b8783ca123ad6e8917df504c0500c6804
Rahmat <srshakir98@gmail.com>
2022-04-28 18:32:38 +0530

    tkinter

--- AuthenticationModule/views.py
+++ AuthenticationModule/views.py
@@ -21,7 +21,7 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_str
 from .tokens import generate_token
 import AuthenticationModule
-import tkinter
+import _tkinter
 
 # Create your views here.
 
